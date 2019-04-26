055f86012b20d13ca134284e90c1b61a8f06c785
james77777778 <20734616+james77777778@users.noreply.github.com>
2019-04-26 13:56:23 +0800

    update

--- DL_HW2/main.py
+++ DL_HW2/main.py
@@ -5,7 +5,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 
-from model import CNN_Net
+from model import Net_O, Net_K, Net_S, Net_F
 
 
 '''
@@ -23,66 +23,76 @@ transforms = transforms.Compose(
 train_dataset = ImageFolder('data/processed/train', transform=transforms)
 valid_dataset = ImageFolder('data/processed/val', transform=transforms)
 train_dataloader = DataLoader(
-    dataset=train_dataset, batch_size=128, shuffle=True, num_workers=3)
+    dataset=train_dataset, batch_size=256, shuffle=True, num_workers=6)
 valid_dataloader = DataLoader(
-    dataset=valid_dataset, batch_size=128, shuffle=False, num_workers=3)
+    dataset=valid_dataset, batch_size=256, shuffle=False, num_workers=6)
 
-model = CNN_Net().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-criterion = torch.nn.CrossEntropyLoss()
+nepoch = 300
 
-nepoch = 50
-loss_record = {'train_loss': [], 'train_acc': [], 'valid_acc': []}
-for epoch in range(nepoch):
-    # train
-    model.train()
-    running_loss = 0
-    correct = 0
-    for batch_idx, (data, target) in enumerate(train_dataloader):
-        data = data.to(device)
-        target = target.to(device)
+model_class = [Net_O, Net_K, Net_S, Net_F]
+for m in model_class:
+    class_name = m.__name__
+    model = m().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    criterion = torch.nn.CrossEntropyLoss()
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-        running_loss += loss.item()*data.size(0)
-        if (batch_idx + 1) % 50 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch,
-                (batch_idx + 1) * len(data), len(train_dataloader.dataset),
-                100. * (batch_idx + 1) / len(train_dataloader), loss.item()))
-    running_loss /= len(train_dataloader.dataset)
-    loss_record['train_loss'].append(running_loss)
-    acc = 100. * float(correct)/len(train_dataloader.dataset)
-    loss_record['train_acc'].append(acc)
-    # valid
-    model.eval()
-    loss = 0
-    correct = 0
-    for batch_idx, (data, target) in enumerate(valid_dataloader):
-        data = data.to(device)
-        target = target.to(device)
+    loss_record = {'train_loss': [], 'train_acc': [], 'valid_acc': []}
+    best_val_acc = 0
+    for epoch in range(nepoch):
+        # train
+        model.train()
+        running_loss = 0
+        correct = 0
+        for batch_idx, (data, target) in enumerate(train_dataloader):
+            data = data.to(device)
+            target = target.to(device)
 
-        output = model(data)
-        loss += F.cross_entropy(output, target, reduction='sum').item()
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+            running_loss += loss.item()*data.size(0)
+            if (batch_idx + 1) % 19 == 0:
+                print('Train Epoch: {} [{}/{}] Loss: {:.6f}'.format(
+                    epoch, (batch_idx + 1) * len(data),
+                    len(train_dataloader.dataset), loss.item()))
+        running_loss /= len(train_dataloader.dataset)
+        loss_record['train_loss'].append(running_loss)
+        acc = 100. * float(correct)/len(train_dataloader.dataset)
+        loss_record['train_acc'].append(acc)
+        # valid
+        model.eval()
+        loss = 0
+        correct = 0
+        for batch_idx, (data, target) in enumerate(valid_dataloader):
+            data = data.to(device)
+            target = target.to(device)
 
-    loss /= len(valid_dataloader.dataset)
-    print('Average loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)'.format(
-        loss, correct, len(valid_dataloader.dataset),
-        100. * float(correct) / len(valid_dataloader.dataset)))
-    acc = 100. * float(correct)/len(valid_dataloader.dataset)
-    loss_record['valid_acc'].append(acc)
+            output = model(data)
+            loss += F.cross_entropy(output, target, reduction='sum').item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-plt.figure()
-plt.plot(loss_record['train_loss'])
-plt.savefig('train_loss.png')
-plt.figure()
-plt.plot(loss_record['train_acc'])
-plt.plot(loss_record['valid_acc'])
-plt.savefig('acc.png')
+        loss /= len(valid_dataloader.dataset)
+        print('{}: Valid avg loss: {:.4f}, Acc: {}/{} ({:.3f}%)'.format(
+            class_name, loss, correct, len(valid_dataloader.dataset),
+            100. * float(correct) / len(valid_dataloader.dataset)))
+        acc = 100. * float(correct)/len(valid_dataloader.dataset)
+        loss_record['valid_acc'].append(acc)
+        if acc > best_val_acc:
+            best_val_acc = acc
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+            }, '{}_best_model'.format(class_name))
+
+    plt.figure()
+    plt.plot(loss_record['train_loss'])
+    plt.savefig('{}_train_loss.png'.format(class_name))
+    plt.figure()
+    plt.plot(loss_record['train_acc'])
+    plt.plot(loss_record['valid_acc'])
+    plt.savefig('{}_acc.png'.format(class_name))
--- DL_HW2/model.py
+++ DL_HW2/model.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 
 
-class CNN_Net(nn.Module):
+class Net_O(nn.Module):
     def __init__(self):
-        super(CNN_Net, self).__init__()
+        super(Net_O, self).__init__()
         self.features = nn.Sequential(
             # 128
             nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
@@ -21,6 +21,105 @@ class CNN_Net(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Linear(64 * 16 * 16, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+# change kernel_size
+class Net_K(nn.Module):
+    def __init__(self):
+        super(Net_K, self).__init__()
+        self.features = nn.Sequential(
+            # 128
+            nn.Conv2d(3, 32, kernel_size=5, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # 63
+            nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # 31
+            nn.Conv2d(64, 64, kernel_size=5, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # 15
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 15 * 15, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+# change stride
+class Net_S(nn.Module):
+    def __init__(self):
+        super(Net_S, self).__init__()
+        self.features = nn.Sequential(
+            # 128
+            nn.Conv2d(3, 32, kernel_size=3, stride=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # 43
+            nn.Conv2d(32, 64, kernel_size=3, stride=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # 15
+            nn.Conv2d(64, 64, kernel_size=3, stride=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # 5
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 5 * 5, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+# change filter size
+class Net_F(nn.Module):
+    def __init__(self):
+        super(Net_F, self).__init__()
+        self.features = nn.Sequential(
+            # 128
+            nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            # 64
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # 32
+            nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # 16
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(32 * 16 * 16, 128),
             nn.BatchNorm1d(128),
             nn.ReLU(),
             nn.Linear(128, 10),
